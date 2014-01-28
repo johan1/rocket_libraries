e57e03340cc24ace063e8bb66a938a7782768545
Johan Viktorsson <johan.viktorsson.dev@gmail.com>
2014-01-28 17:32:13 +0100

    Forgot to add vorbis

---
+++ libvorbis/dependencies.mk
@@ -0,0 +1,3 @@
+LOCAL_LIBNAME 		:= ogg
+LOCAL_LIBVERSION	:= 1.3.0
+include $(BUILD_ROOT)/common/repo_dependency.mk
---
+++ libvorbis/project.mk
@@ -0,0 +1,37 @@
+# Required
+NAME := vorbis
+TYPE := static
+
+SOURCE_DIR := $(PROJECT_ROOT)/src/lib
+
+INCLUDES += -I$(PROJECT_ROOT)/src/include
+INCLUDES += -I$(PROJECT_ROOT)/src/lib
+
+CSOURCES := \
+	analysis.c \
+	bitrate.c \
+	block.c \
+	codebook.c \
+	envelope.c \
+	floor0.c \
+	floor1.c \
+	info.c \
+	lookup.c \
+	lpc.c \
+	lsp.c \
+	mapping0.c \
+	mdct.c \
+	psy.c \
+	registry.c \
+	res0.c \
+	sharedbook.c \
+	smallft.c \
+	synthesis.c \
+	vorbisenc.c \
+	vorbisfile.c \
+	window.c
+
+# Deploy
+EXPORT_INCLUDE_FOLDER_NAME := vorbis
+EXPORT_LIBNAME := vorbis
+EXPORT_LIBVERSION := 1.3.3
