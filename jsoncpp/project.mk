f63c339843c3d833384176a58cfc74fe1e10af85
Johan Viktorsson <johan.viktorsson.dev@gmail.com>
2014-07-27 19:55:47 +0200

    Added jsoncpp as third party library
    cppjson is GPL we needed an alternative.

---
+++ jsoncpp/project.mk
@@ -0,0 +1,10 @@
+# Required
+NAME := jsoncpp
+TYPE := static
+
+# Since we have the includes in the same folder as source
+INCLUDES += -I$(PROJECT_ROOT)/include
+
+# Deploy
+EXPORT_LIBNAME := jsoncpp
+EXPORT_LIBVERSION := 0.5.0
